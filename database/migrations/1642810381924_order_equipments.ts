26d241bbd1fdb1f4e1ba3cfbb77de2ba3126360c
Ateeq <ateeg1999@gmail.com>
2022-01-22 17:02:28 +0200

    add new admin seeder and edit migration file

--- database/migrations/1642810381924_order_equipments.ts
+++ database/migrations/1642810381924_order_equipments.ts
@@ -6,8 +6,8 @@ export default class OrdereEuipments extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('equipment_id').references('equipments.id').onDelete("CASCADE")
-      table.string('order_id').references('orders.id').onDelete("CASCADE")
+      table.string('equipment_id').notNullable()
+      table.string('order_id').notNullable()
       table.integer("quantity")
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
